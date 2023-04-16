377e767186ab4f79b698e19a12c253201209ee8b
ABRAHAM LEANDRO VEGA <leandrovegaabraham@gmail.com>
2023-04-16 03:48:22 -0500

    get api data product configured

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -1,21 +1,35 @@
+import { getDiscountedPricePercentage } from "@/utils/helper";
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-function ProductCard() {
+function ProductCard({ data: { attributes: p, id } }) {
   return (
     <Link
-      href="/product/1"
+      href={`/products/${p.slug}`}
       className="transform overflow-hidden bg-white duration-200 hover:scale-105 cursor-pointer"
     >
-      <img className="w-full" src="/product-1.webp" alt="Product image" />
+      <Image
+        width={500}
+        height={500}
+        src={p.thumbnail.data?.attributes.url}
+        alt={p.name}
+      />
       <div className="p-4 text-black/[0.9]">
-        <h2 className="text-lg font-medium">Product name</h2>
+        <h2 className="text-lg font-medium">{p.name}</h2>
         <div className="flex items-center text-black/[0.5]">
-          <p className="mr-2 text-lg font-semibold">$250.00</p>
-          <p className="text-base font-medium line-through">$25.00</p>
-          <p className="ml-auto text-base font-medium text-green-500">
-            20% off
-          </p>
+          <p className="mr-2 text-lg font-semibold">&#8377;{p.price}</p>
+
+          {p.original_price && (
+            <>
+              <p className="text-base font-medium line-through">
+                &#8377;{p.original_price}
+              </p>
+              <p className="ml-auto text-base font-medium text-green-500">
+                {getDiscountedPricePercentage(p.original_price, p.price)}%off
+              </p>
+            </>
+          )}
         </div>
       </div>
     </Link>
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import HeroBanner from "@/components/HeroBanner";
 import ProductCard from "@/components/ProductCard";
 import Wrapper from "@/components/Wrapper";
+import { fetchDataFromApi } from "@/utils/api";
 
-export default function Home() {
+export default function Home({ products }) {
   return (
     <>
       <main>
@@ -16,18 +17,20 @@ export default function Home() {
           </div>
 
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
+            {products?.data?.map((product) => (
+              <ProductCard key={product?.id} data={product} />
+            ))}
           </div>
         </Wrapper>
       </main>
     </>
   );
+}
+
+export async function getStaticProps() {
+  const products = await fetchDataFromApi("/api/products?populate=*");
+
+  return {
+    props: { products },
+  };
 }
